574b6243e363acceb49a9890b1d9fcdad53d5df0
alanne.soares <alannesilva@yahoo.com.br>
2017-04-14 23:20:13 -0300

    Alterado

--- Alura/JavaScript/projeto002/js/principal.js
+++ Alura/JavaScript/projeto002/js/principal.js
@@ -1,3 +1,20 @@
-//console.log("Fui carregado!");
+/* Para exibir no console...
+
+console.log("Fui carregado!");
+
+*/
+
+/* Variáve...
+
 var titulo = document.querySelector("h1");
 titulo.textContent = "Aparecida Nutricionista";
+
+*/
+
+
+/* Para declarar uma variável e exibir em pop up
+
+var mensagem = "Sejam Bem - Vindos!";
+alert(mensagem);
+
+*/
